3679786c887c00a31a9b86300578a6a063ed6ae9
mrcxlinux <mrcxlinux@proton.me>
2025-06-10 21:53:07 +0300

    unica: patches: miscs: Add ProjectScarlet branding and Build ID formatting

--- unica/patches/miscs/customize.sh
+++ unica/patches/miscs/customize.sh
@@ -2,7 +2,7 @@ TARGET_FIRMWARE_PATH="$FW_DIR/$(echo -n "$TARGET_FIRMWARE" | sed 's./._.g' | rev
 
 # Set build ID
 VALUE="$(GET_PROP "$WORK_DIR/system/system/build.prop" "ro.build.display.id")"
-SET_PROP "system" "ro.build.display.id" "EternityROM $ROM_VERSION - ($VALUE)"
+SET_PROP "system" "ro.build.display.id" "ProjectScarlet-$ROM_VERSION_$VALUE"
 
 # Disable FRP
 SET_PROP "vendor" "ro.frp.pst" ""
