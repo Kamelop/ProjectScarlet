152998185cac23717f769c6cda5079b7a510078b
mrcxlinux <mrcxlinux@proton.me>
2025-06-10 22:07:58 +0300

    platform: exynos9810: patches: miscs: create & populate

---
+++ platform/exynos9810/patches/miscs/customize.sh
@@ -0,0 +1,16 @@
+echo "Disabling A/B"
+SET_PROP "product" "ro.product.ab_ota_partitions" --delete
+
+echo "Disabling UFFD GC"
+SET_PROP "product" "ro.dalvik.vm.enable_uffd_gc" "false"
+
+echo "Disabling A2DP Offload"
+SET_PROP "system" persist.bluetooth.a2dp_offload.disabled "true"
+
+echo "Setting SF flags"
+SET_PROP "vendor" "debug.sf.latch_unsignaled" "1"
+SET_PROP "vendor" "debug.sf.high_fps_late_app_phase_offset_ns" "0"
+SET_PROP "vendor" "debug.sf.high_fps_late_sf_phase_offset_ns" "0"
+
+echo "Enabling updateable APEX images"
+SET_PROP "vendor" "ro.apex.updatable" "true"
