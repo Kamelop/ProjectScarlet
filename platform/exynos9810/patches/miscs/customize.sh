39861f43784df704976195c386d0517d190ca050
mrcxlinux <mrcxlinux@proton.me>
2025-06-10 22:12:24 +0300

    platform: exynos9810: patches: miscs: Disable HFR

--- platform/exynos9810/patches/miscs/customize.sh
+++ platform/exynos9810/patches/miscs/customize.sh
@@ -14,3 +14,7 @@ SET_PROP "vendor" "debug.sf.high_fps_late_sf_phase_offset_ns" "0"
 
 echo "Enabling updateable APEX images"
 SET_PROP "vendor" "ro.apex.updatable" "true"
+
+echo "Disabling HFR"
+SET_PROP "vendor" "ro.surface_flinger.enable_frame_rate_override" "false"
+SET_PROP "vendor" "ro.surface_flinger.use_content_detection_for_refresh_rate" "false"
