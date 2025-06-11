2875345f5e5dbf5032ab1575e2ba9793473755a6
mrcxlinux <mrcxlinux@proton.me>
2025-06-11 19:01:11 +0300

    buildenv: replace /bin/pwd with pwd

--- buildenv.sh
+++ buildenv.sh
@@ -24,13 +24,13 @@ _GET_SRC_DIR()
     local TOPFILE="unica/config.sh"
     if [ -n "$SRC_DIR" ] && [ -f "$SRC_DIR/$TOPFILE" ]; then
         # The following circumlocution ensures we remove symlinks from SRC_DIR.
-        (cd "$SRC_DIR"; PWD= /bin/pwd)
+        (cd "$SRC_DIR"; PWD= pwd)
     else
         if [ -f "$TOPFILE" ]; then
             # The following circumlocution (repeated below as well) ensures
             # that we record the true directory name and not one that is
             # faked up with symlink names.
-            PWD= /bin/pwd
+            PWD= pwd
         else
             local HERE="$PWD"
             local T=
